e99d7da746a2116d8d87cf9c271477b2c1f2e1ad
lanleilin <2476081528@qq.com>
2017-10-25 00:27:30 +0800

    work link
    Change-Id: I0509aebbc65ebce1fe8fa215088348e3a3aa8b60

---
+++ form/js/workAnchorList.js
@@ -0,0 +1,17 @@
+var anchorList = [{
+    tag: 'demo',
+    name: '简单webrtcdemo',
+    url: 'http://webrtc.org.cn/tutorial-simple-video-chat/'
+}, {
+    name: 'h5demo',
+    url: 'https://h5.anychat.net.cn/demo/'
+}, {
+    name: '坐席端',
+    url: 'https://h5test.anychat.net.cn/admin_agent/#/Login/'
+}, {
+    name: 'h5开户客户端',
+    url: 'https://h5.anychat.net.cn/v-c/#/Loginpc/'
+}, {
+    name: '视频域h5demo',
+    url: 'https://h5.anychat.net.cn/h5demo/standard.html'
+}]
